2737303185ba47eb0cde2d62d6a7bd09cc8a1193
Ali Punhal <71116445+Alipunhalbaloch@users.noreply.github.com>
2024-11-12 18:15:56 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const userRoutes =require("./routes/userRoutes");
 const chatRoutes= require("./routes/chatRoutes")
 const messageRoutes =require("./routes/messageRoutes")
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
-
+const path= require('path');
 
 dotenv.config()
 connectDB();
@@ -21,7 +21,12 @@ app.use(express.json()); // to accept json data
 app.use("/api/user",userRoutes);
 app.use('/api/chat',chatRoutes);
 app.use('/api/message',messageRoutes);
-
+// ------------deployment-----------
+const __dirname1=path.resolve()
+if(process.env.NODE_ENV === 'production'){
+    
+}
+// ------------deployment-----------
 
 app.use(notFound);
 app.use(errorHandler);
